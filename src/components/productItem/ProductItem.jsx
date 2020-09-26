827f6dbfa1fbcabde0ad0534a2283046cf2efc71
DanielStefan11 <dannielstefan592@gmail.com>
2020-09-26 14:48:31 +0300

    Refactoring components & pages files

--- src/components/productItem/ProductItem.jsx
+++ src/components/productItem/ProductItem.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./ProductItem.css";
 import { connect } from "react-redux";
-import { addToCart } from "../redux/cart/CartActions";
+import { addToCart } from "../../redux/cart/CartActions";
 import { Link } from "react-router-dom";
 
 function ProductItem(props) {
--- src/pages/productPage/Product.jsx
+++ src/pages/productPage/Product.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import Layout from "../components/Layout";
-import products from "../utils/products.json";
+import Layout from "../../components/layout/Layout";
+import products from "../../utils/products.json";
 import "./Product.css";
 import { connect } from "react-redux";
-import { addToCart } from "../redux/cart/CartActions";
+import { addToCart } from "../../redux/cart/CartActions";
 
 class Product extends React.Component {
   constructor(props) {
